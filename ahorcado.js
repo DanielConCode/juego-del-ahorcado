8ef33272a535f25171646abe45d8671435568e86
Daniel Con <esau02con@gmail.com>
2022-10-27 20:54:01 -0600

    Separando el codigo para dibujar la horca en ahorcado.js

---
+++ ahorcado.js
@@ -0,0 +1,69 @@
+var pantalla = document.querySelector(".canvas-ahorcado");
+
+//Funcion que dibujara la horca
+function DibujarHorca(errores)
+{
+    switch(errores)
+    {
+        case 9:
+            DibujarTrazo(225,275,25,275); //Dibujar la base del dibujo del ahorcado 
+        break;
+        case 8:
+            DibujarTrazo(75,275,75,25); //Dibuja el primer tramo al equivocarse 
+        break;
+        case 7:
+            DibujarTrazo(70,25,180,25);
+        break;
+        case 6:
+            DibujarTrazo(175,25,175,50);
+        break;        
+        case 5:
+            //Dibuja el cuarto tramo al equivocarse, este es una circunferencia, la cabeza del ahorcado
+            var pincel = pantalla.getContext("2d");
+            pincel.strokeStyle = "darkblue";
+            pincel.beginPath();
+            pincel.arc(175, 75, 25, 0, 2*3.14);
+            pincel.stroke(); 
+            pincel.closePath();
+        break;
+        case 4:
+            DibujarTrazo(175,100,175,175);
+        break;
+        case 3:
+            DibujarTrazo(175,100,150,140);
+        break;
+        case 2:
+            DibujarTrazo(175,100,200,140);
+        break;
+        case 1:
+            DibujarTrazo(175,170,150,200);
+        break;
+        case 0:
+            DibujarTrazo(175,170,200,200);
+            DibujarTexto("red", "Fin del juego!", 250, 150)
+        break;
+    }
+
+}
+
+//Funcion para dibujar los trazos del ahorcado
+function DibujarTrazo(x1, y1, x2, y2)
+{
+    var pincel = pantalla.getContext("2d");
+    pincel.strokeStyle = "darkblue";
+    pincel.beginPath();
+    pincel.lineWidth = 10;
+    pincel.moveTo(x1,y1); //Punto inicial 
+    pincel.lineTo(x2,y2); //Punto final
+    pincel.stroke(); 
+    pincel.closePath();
+}
+
+//Funcion que dibujara el texto para la canva-ahorcado en el color y posicion que se indique en los parametros 
+function DibujarTexto(color, texto, x, y)
+{
+    var pincel = pantalla.getContext("2d");
+    pincel.fillStyle = color;
+    pincel.font = "58px Shalimar";
+    pincel.fillText(texto, x, y,);
+}
--- script.js
+++ script.js
@@ -11,15 +11,12 @@ var errores = 9;
 var palabraSecreta;
 
 //Variable Pantalla que toma como referencia cada canva
-var pantalla = document.querySelector(".canvas-ahorcado");
 var pantallaLetras = document.querySelector(".canvas-letras");
-
-//variable Pincel que toma nuestro contexto para dibujar en la canva
-var pincel = pantalla.getContext("2d");
 
 //Funcion que inicia el juego al presionar el boton Empezar Juego
 function IniciarJuego()
 {
+    DibujarHorca(errores);
     EscogerPalabra();
     dibujarLineas();
     document.querySelector(".boton-empezar-juego").style.display = "none";
@@ -119,7 +116,7 @@ function EscribirLetraIncorrecta(letra, errorsLeft)
 
     var anchura = 600 / palabraSecreta.length;
 
-    pincel.fillText(letra, 35+(40*(10-errorsLeft)), 100, 40);
+    pincel.fillText(letra, (40*(10-errorsLeft)), 100, 40);
 }
 
 //Funcion que resta los intentos
@@ -127,57 +124,5 @@ function AnadirLetraIncorrecta()
 {
     errores = errores - 1;
     console.log(errores);
-}
-
-
-//Dibujar el ahorcado:
-
-//STROKE dibuja el tramo trazado, con StrokeStyle definimos el color del trazo
-pincel.strokeStyle = "darkblue";
-
-//Dibujar la base del dibujo del ahorcado 
-DibujarTrazo(225,275,25,275);
-
-//Dibuja el primer tramo al equivocarse 
-DibujarTrazo(75,275,75,25);
-
-//Dibuja el segundo tramo al equivocarse 
-DibujarTrazo(70,25,180,25);
-
-//Dibuja el tercer tramo al equivocarse 
-DibujarTrazo(175,25,175,50);
-
-//Dibuja el tercer tramo al equivocarse 
-DibujarTrazo(175,25,175,50);
-
-//Dibuja el cuarto tramo al equivocarse, este es una circunferencia, la cabeza del ahorcado
-pincel.beginPath();
-pincel.arc(175, 75, 25, 0, 2*3.14);
-pincel.stroke(); 
-
-
-//Dibuja el quinto tramo al equivocarse 
-DibujarTrazo(175,100,175,175);
-
-//Dibuja el sexto tramo al equivocarse 
-DibujarTrazo(175,100,150,140);
-
-//Dibuja el septimo tramo al equivocarse 
-DibujarTrazo(175,100,200,140);
-
-//Dibuja el octavo tramo al equivocarse 
-DibujarTrazo(175,170,150,200);
-
-//Dibuja el noveno tramo al equivocarse 
-DibujarTrazo(175,170,200,200);
-
-//Funcion para dibujar los trazos del ahorcado
-function DibujarTrazo(x1, y1, x2, y2)
-{
-    pincel.beginPath();
-    pincel.lineWidth = 10;
-    pincel.moveTo(x1,y1); //Punto inicial 
-    pincel.lineTo(x2,y2); //Punto final
-    pincel.stroke(); 
-    pincel.closePath();
+    DibujarHorca(errores)
 }
